0744da322da4d0c7e3b932217333ef73075f5b90
woodsaj <anthony@monkey.id.au>
2016-10-26 21:17:30 +0800

    copy binaries from correct path

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ mkdir -p ${BASE}/build/usr/bin
 cd $GOPATH/src/github.com/intelsdi-x/snap
 make deps
 make snap
-cp ./build/bin/* ${BASE}/build/usr/bin/
+cp ./build/linux/x86_64/* ${BASE}/build/usr/bin/
 
